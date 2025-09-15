7fcee84ce056664b0ce883e70ece565f63e1936d
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 10:12:45 +0530

    Update movies.js

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,17 +1,46 @@
 const movies = [
   {
     id: 1,
-    title: "Sample Movie 1",
-    overview: "This is a sample description.",
-    poster: "https://picsum.photos/200/300?1",
-    backdrop: "https://picsum.photos/1280/720?1"
+    title: "Harry Potter and the Sorcerer's Stone",
+    overview: "An orphaned boy discovers he is a wizard and attends Hogwarts School of Witchcraft and Wizardry.",
+    poster: "https://m.media-amazon.com/images/I/81YOuOGFCJL._AC_SY679_.jpg",
+    backdrop: "https://m.media-amazon.com/images/I/71pye00RxSL._AC_SL1500_.jpg"
   },
   {
     id: 2,
-    title: "Sample Movie 2",
-    overview: "This is a sample description.",
-    poster: "https://picsum.photos/200/300?2",
-    backdrop: "https://picsum.photos/1280/720?2"
+    title: "The Conjuring",
+    overview: "Paranormal investigators Ed and Lorraine Warren work to help a family terrorized by a dark presence.",
+    poster: "https://m.media-amazon.com/images/I/71Y1+kMJtUL._AC_SY679_.jpg",
+    backdrop: "https://m.media-amazon.com/images/I/81E3XKp3FjL._AC_SL1500_.jpg"
+  },
+  {
+    id: 3,
+    title: "The Nun",
+    overview: "A priest and a novice are sent by the Vatican to investigate the death of a young nun in Romania.",
+    poster: "https://m.media-amazon.com/images/I/81oTv6BzQzL._AC_SY679_.jpg",
+    backdrop: "https://m.media-amazon.com/images/I/91o+KgpFKnL._AC_SL1500_.jpg"
+  },
+  {
+    id: 4,
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    overview: "A meek Hobbit and eight companions set out on a journey to destroy the One Ring and save Middle-earth.",
+    poster: "https://m.media-amazon.com/images/I/51Qvs9i5a%2BL._AC_.jpg",
+    backdrop: "https://m.media-amazon.com/images/I/81tCWi7%2B2sL._AC_SL1500_.jpg"
+  },
+  {
+    id: 5,
+    title: "The Lord of the Rings: The Two Towers",
+    overview: "While Frodo and Sam edge closer to Mordor, the divided fellowship makes a stand against Sauron's forces.",
+    poster: "https://m.media-amazon.com/images/I/51EstVXM1UL._AC_.jpg",
+    backdrop: "https://m.media-amazon.com/images/I/81mL8QvfWfL._AC_SL1500_.jpg"
+  },
+  {
+    id: 6,
+    title: "The Lord of the Rings: The Return of the King",
+    overview: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw attention from Frodo and Sam.",
+    poster: "https://m.media-amazon.com/images/I/51Qvs9i5a%2BL._AC_.jpg",
+    backdrop: "https://m.media-amazon.com/images/I/91N0E2YQoIL._AC_SL1500_.jpg"
   }
 ];
+
 export default movies;
