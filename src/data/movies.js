c6454dbf2d2557bc7bba32e6078069c9a9375483
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 10:02:06 +0530

    Create movies.js

---
+++ src/data/movies.js
@@ -0,0 +1,17 @@
+const movies = [
+  {
+    id: 1,
+    title: "Sample Movie 1",
+    overview: "This is a sample description.",
+    poster: "https://picsum.photos/200/300?1",
+    backdrop: "https://picsum.photos/1280/720?1"
+  },
+  {
+    id: 2,
+    title: "Sample Movie 2",
+    overview: "This is a sample description.",
+    poster: "https://picsum.photos/200/300?2",
+    backdrop: "https://picsum.photos/1280/720?2"
+  }
+];
+export default movies;
