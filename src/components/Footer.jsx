d73b5948596956589bee0f47aa7d4146d8c532ba
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 13:13:28 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer style={{ padding: '20px', background: '#111', color: 'white', textAlign: 'center' }}>
+      <p>© 2025 Netflix Clone</p>
+    </footer>
+  );
+}
