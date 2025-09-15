56e949053f3418474f7050b5320ae731d7563f7b
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 10:03:19 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="nav">
+      <div className="logo">MyFlix</div>
+      <div>User</div>
+    </div>
+  )
+}
